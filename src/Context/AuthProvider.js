f053de4d10f453d2ff70648ccfeb33688e09a783
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-11-25 22:17:54 +0600

    reload issu solved

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -8,18 +8,22 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState('')
+    const [loading, setLoading] = useState(true);
     // const user = {displayName: 'Jerry'}
 
     //create user 
     const createUser = (email, password)=>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
     // login user 
     const userLogin = (email, password)=>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
     // log out user 
     const userLogout = () => {
+        setLoading(true)
         return signOut(auth)
     }
 
@@ -27,15 +31,17 @@ const AuthProvider = ({children}) => {
     useEffect(()=> {
         const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
             setUser(currentUser);
+            setLoading(false)
         })
 
         return ()=>{
             unSubscribe()
         }
-    },[])
+    },[setLoading])
 
     const authInfo = {
         user,
+        loading,
         createUser,
         userLogin,
         userLogout
--- src/Routes/PrivetRoute.js
+++ src/Routes/PrivetRoute.js
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthProvider';
+import Loading from '../Shared/Loading/Loading';
 
 const PrivetRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+    const {user,loading} = useContext(AuthContext);
     const location = useLocation();
+    if(loading){
+        return <Loading></Loading>
+    }
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
