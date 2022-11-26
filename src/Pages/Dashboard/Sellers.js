67b7af444fbc7c7ebc0f046de1d05df94317ee6a
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-11-26 20:41:31 +0600

    showing only sellers on sellers route

--- src/Pages/Dashboard/Sellers.js
+++ src/Pages/Dashboard/Sellers.js
@@ -2,13 +2,54 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Sellers = () => {
-    // const {} = useQuery({
-    //     queryKey: ['sellers'],
-    //     queryFn: ()=>fetch('')
-    // })
+    const {data: sellers = []} = useQuery({
+        queryKey: ['sellers'],
+        queryFn: ()=>fetch('http://localhost:5000/sellers')
+        .then(res => res.json())
+    })
+    console.log(sellers)
     return (
         <div>
-            <h1>Sellers </h1>
+            <h1 className='text-center text-3xl font-semibold'>Sellers </h1>
+
+            <div>
+                
+                   <div  className="overflow-x-auto">
+                        <table className="table w-full">
+                           
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>email</th>
+                                    <th>Account status</th>
+                                    <th>Make admin</th>
+                                    <th>delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    sellers.map((user , i) => <tr key={user._id}>
+                                        <th>{i+1}</th>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.account}</td>
+                                        <td><button className='btn btn-sm'>Verify </button></td>
+                                        <td><button className='btn btn-sm'>Delete </button></td>
+                                    </tr>)
+                                        
+                                }
+                               
+                                
+                               
+                                
+                            
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                
+            </div>
         </div>
     );
 };
