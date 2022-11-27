e31901c46abf7be117210083c44b7600764fb280
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-11-27 23:45:15 +0600

    the buyer will see only his/her order on My order route

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -11,65 +11,56 @@ const AllUsers = () => {
             .then(res => res.json())
     })
     // console.log(users)
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
 
-    const handleMakeAdmin = (_id)=>{
+    const handleMakeAdmin = (_id) => {
         // console.log(_id)
-        fetch(`http://localhost:5000/user/${_id}`,{
+        fetch(`http://localhost:5000/user/${_id}`, {
             method: 'PUT'
         })
-        .then(res => res.json())
-        .then(data => {
-            if(data.modifiedCount > 0){
-                toast.success('admin make successfully')
-                refetch()
-            }
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('admin make successfully')
+                    refetch()
+                }
+                console.log(data)
+            })
     }
     return (
         <div>
             <h1 className='text-center text-3xl font-semibold'>All users </h1>
 
             <div>
-                
-                   <div  className="overflow-x-auto">
-                        <table className="table w-full">
-                           
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Name</th>
-                                    <th>email</th>
-                                    <th>Account status</th>
-                                    <th>Make admin</th>
-                                    
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    users.map((user , i) => <tr key={user._id}>
-                                        <th>{i+1}</th>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.account}</td>
-                                        <td>{user?.role !== 'admin' ? <button onClick={()=>handleMakeAdmin(user._id)} className='btn btn-sm'>make admin</button> : <span>Admin </span>}</td>
-                                        
-                                    </tr>)
-                                        
-                                }
-                               
-                                
-                               
-                                
-                            
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>email</th>
+                                <th>Account status</th>
+                                <th>Make admin</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, i) => <tr key={user._id}>
+                                    <th>{i + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.account}</td>
+                                    <td>{user?.role !== 'admin' ? <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-sm'>make admin</button> : <span>Admin </span>}</td>
+
+                                </tr>)
+
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     );
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const MyOrders = () => {
+    const {user} = useContext(AuthContext);
+
+
+    const {data: orders =[],} = useQuery({
+        queryKey: ['orders'],
+        queryFn:()=>fetch(`http://localhost:5000/orders/${user?.email}`)
+        .then(res => res.json())
+    })
+    // console.log(orders)
     return (
         <div>
             <h1>My orders </h1>
+
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Item name</th>
+                                <th>Price</th>
+                                <th>Meeting location</th>
+                                <th>Payment</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                orders.map((order, i) => <tr key={order._id}>
+                                    <th>{i + 1}</th>
+                                    <td>{order.itemName}</td>
+                                    <td>{order.price}</td>
+                                    <td>{order.meetingLocation}</td>
+                                    <td><button className='btn btn-primary btn-sm'>Pay</button></td>
+
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
     );
 };
